3dd6433efdd463ab47cc763d01045f02dabe2e1e
uuuuu1226 <uuuuu12261@naver.com>
2018-10-20 02:30:49 +0900

    Add files
    Add files

---
+++ app/src/test/java/com/uuuuu1226/seoul/eunseok/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.uuuuu1226.seoul.eunseok;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
